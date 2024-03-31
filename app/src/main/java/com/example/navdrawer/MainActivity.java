b6c9891d0f9a2a2e25d27415c0a12f1fb08313ea
Ocean TheSea <oceanmuryanto0@gmail.com>
2024-03-31 14:15:27 +0700

    NavDrawer ver1~

---
+++ app/src/main/java/com/example/navdrawer/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.navdrawer;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.bumptech.glide.Glide;
+import com.example.navdrawer.fragments.Makanan_Fav_Fragment;
+import com.example.navdrawer.fragments.Makanan_Fragment;
+import com.example.navdrawer.fragments.Minuman_Fragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        showHomePage();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.menu_makanan) {
+                    showHomePage();
+                    getSupportActionBar().setTitle("Makanan Page");
+                }
+                else if (itemId == R.id.menu_minuman) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_frame, new Minuman_Fragment())
+                            .commit();
+                    getSupportActionBar().setTitle("Minuman Page");
+                } else {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_frame, new Makanan_Fav_Fragment())
+                            .commit();
+                    getSupportActionBar().setTitle("MakananFav Page");
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        ImageView imageView = navigationView.getHeaderView(0).findViewById(R.id.imageViewProfile);
+
+        String imageUrl = "https://i.pinimg.com/736x/ea/5c/5c/ea5c5cec244c144c1b2e9de01866907f.jpg";
+        Glide.with(this)
+                .load(imageUrl)
+                .into(imageView);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();
+    }
+
+    private void showHomePage() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new Makanan_Fragment())
+                .commit();
+
+        getSupportActionBar().setTitle("Home Page");
+    }
+}
---
+++ app/src/main/java/com/example/navdrawer/fragments/Makanan_Fav_Fragment.java
@@ -0,0 +1,25 @@
+package com.example.navdrawer.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.navdrawer.R;
+
+public class Makanan_Fav_Fragment extends Fragment {
+
+    public Makanan_Fav_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_makananfav, container, false);
+    }
+
+}
