eb31c390a81911431d772a07bd1164adcdb332a1
Ocean TheSea <oceanmuryanto0@gmail.com>
2024-04-01 11:41:16 +0700

    NavDrawer ver2~

---
+++ app/src/main/java/com/example/navdrawer/Item.java
@@ -0,0 +1,37 @@
+package com.example.navdrawer;
+
+public class Item {
+
+    String name, desc, image;
+
+    public Item() {
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
---
+++ app/src/main/java/com/example/navdrawer/MyAdapter.java
@@ -0,0 +1,4 @@
+package com.example.navdrawer;
+
+public class MyAdapter {
+}
